6334e750febf174598457c7bcfab9487c4fdc858
Anushka Paul <anushkapaul1103@gmail.com>
2020-10-19 21:16:45 +0530

    Updates

---
+++ precache-manifest.88a64879dbe8df53a991bfc3cad07add.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f464b1273057584c3f816d7ef487502",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "0427215266eddaf878cf",
+    "url": "/covid_tracker/static/css/main.59a8af70.chunk.css"
+  },
+  {
+    "revision": "4ef752b78ae63deaae0d",
+    "url": "/covid_tracker/static/js/2.a73a1391.chunk.js"
+  },
+  {
+    "revision": "639c28c8ef14bcd37d017fe8f6dc04e2",
+    "url": "/covid_tracker/static/js/2.a73a1391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0427215266eddaf878cf",
+    "url": "/covid_tracker/static/js/main.d10a30a7.chunk.js"
+  },
+  {
+    "revision": "4c5145b2abee34e7044b",
+    "url": "/covid_tracker/static/js/runtime-main.fb184295.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
